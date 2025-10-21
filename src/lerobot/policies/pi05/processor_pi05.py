1be968fc8e3a60a1c004c9853004440269564473
Darko Lukic <lukicdarkoo@gmail.com>
2025-10-21 14:50:14 +0200

    Implement delta action processor

--- src/lerobot/policies/pi05/processor_pi05.py
+++ src/lerobot/policies/pi05/processor_pi05.py
@@ -44,6 +44,37 @@ from lerobot.utils.constants import (
     POLICY_PREPROCESSOR_DEFAULT_NAME,
 )
 
+from dataclasses import dataclass
+from lerobot.processor import (
+    EnvTransition,
+    ProcessorStep,
+    ProcessorStepRegistry,
+    TransitionKey,
+)
+from lerobot.configs.types import PipelineFeatureType, PolicyFeature
+
+
+@ProcessorStepRegistry.register("delta_actions_processor")
+@dataclass
+class DeltaActionsProcessorStep(ProcessorStep):
+    def __call__(self, transition: EnvTransition) -> EnvTransition:
+        self._current_transition = transition.copy()
+        new_transition = self._current_transition
+
+        action = new_transition.get(TransitionKey.ACTION)
+        observation = new_transition.get(TransitionKey.OBSERVATION)
+
+        if action is not None and observation is not None:
+            obs_state = new_transition[TransitionKey.OBSERVATION]["observation.state"]
+            new_transition[TransitionKey.ACTION][:, :, :6] -= obs_state.unsqueeze(1)[:, :, :6]
+
+        return new_transition
+
+    def transform_features(
+        self, features: dict[PipelineFeatureType, dict[str, PolicyFeature]]
+    ) -> dict[PipelineFeatureType, dict[str, PolicyFeature]]:
+        return features
+
 
 @ProcessorStepRegistry.register(name="pi05_prepare_state_tokenizer_processor_step")
 @dataclass
@@ -131,6 +162,7 @@ def make_pi05_pre_post_processors(
 
     # Add remaining processors
     input_steps: list[ProcessorStep] = [
+        DeltaActionsProcessorStep(),
         RenameObservationsProcessorStep(rename_map={}),  # To mimic the same processor as pretrained one
         AddBatchDimensionProcessorStep(),
         # NOTE: NormalizerProcessorStep MUST come before Pi05PrepareStateTokenizerProcessorStep
